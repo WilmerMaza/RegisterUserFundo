97b6b3c4995ef94f1990950dba4b9efbff1c2379
Ale13258 <aleja13258@gmail.ocm>
2024-06-30 23:12:19 -0500

    sin comentarios

--- src/api/api.routes.ts
+++ src/api/api.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import  {addAthlete } from "../api/controller/AthleteController";
-//import { login, register } from "./Controllr/authController";
+
 
 const authRoutes = Router();
 
 authRoutes.post('/', addAthlete);
 
-//authRoutes.post('/login', login);
 
 export default authRoutes;
